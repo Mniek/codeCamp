1ce3ad23443a1275b69834ea88ac310a6a3eb5f0
Mniek <mniek890@o2.pl>
2019-05-05 13:09:00 +0200

    CodeCamp

---
+++ shower.js
@@ -0,0 +1,10 @@
+function showCode( place,desc)
+{ 
+    var newItem = document.createElement("p");
+    var textnode = document.createTextNode("Water");
+    newItem.appendChild(textnode);
+    console.log(this)
+    // var list = ;
+    list.insertBefore(newItem, this);
+   
+}
